b36555d2552ebc032d52fecdd32a803a59aa8d32
Neetu99 <120268912+Neetu99@users.noreply.github.com>
2023-04-25 14:38:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-//Write your code here
+class API{
+    #secure;
+    url;
+    method;
+    constructor(url){
+        this.url=url;
+        if(url.includes("https")) this.#secure=true;
+        else this.#secure=false;
+        this.method="GET"
+    }
+    isSecure(){
+        return this.#secure;
+    }
+    updateUrl(url){
+        this.url=url
+        if(url.includes("https"))
+        this.#secure=true;
+        else
+        this.#secure=false;
+    }
+}
 
-
-
-
-
-//Do not remove this code
 module.exports = { API }
